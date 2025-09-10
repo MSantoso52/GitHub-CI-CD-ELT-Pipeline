bb95a78ee930dd520875e63a1ce133d3550950d3
MSantoso52 <santosomulyo52@gmail.com>
2025-09-10 11:26:36 +0700

    Update elt_dag_test.py

--- test/elt_dag_test.py
+++ test/elt_dag_test.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from airflow.models import DagBag
 from dags.elt_dag import extract_and_load, dag  # Import your DAG and functions
